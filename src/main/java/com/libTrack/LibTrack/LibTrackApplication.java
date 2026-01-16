b7c9f0299c67aa4d50d2e1af792c19a4aa7152bb
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-01-16 15:21:23 -0300

    feat: Iniciado o projeto LibTrack

---
+++ src/main/java/com/libTrack/LibTrack/LibTrackApplication.java
@@ -0,0 +1,13 @@
+package com.libTrack.LibTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libTrack/LibTrack/LibTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libTrack.LibTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
