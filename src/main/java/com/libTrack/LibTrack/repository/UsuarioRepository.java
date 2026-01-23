6ca6744a9ef4fb5601b9250983b1d9841b1cc388
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-01-22 23:16:51 -0300

    feat: Criado o repository da Entidade Usuario

---
+++ src/main/java/com/libTrack/LibTrack/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.libTrack.LibTrack.repository;
+
+import com.libTrack.LibTrack.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByMatricula(Long matricula);
+}
