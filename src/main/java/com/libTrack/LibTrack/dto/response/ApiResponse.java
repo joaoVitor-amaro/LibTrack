696be4ecb2f2d358319c52b6f57036b76dc39ca9
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-01-22 23:30:13 -0300

    feat: Modelo padrão de response de uma requisição

---
+++ src/main/java/com/libTrack/LibTrack/dto/response/ApiResponse.java
@@ -0,0 +1,36 @@
+package com.libTrack.LibTrack.dto.response;
+
+import com.libTrack.LibTrack.dto.BIbliotecarioDTO;
+import com.libTrack.LibTrack.model.Bibliotecario;
+
+import java.time.LocalDateTime;
+
+public class ApiResponse<T> {
+    private int code;
+    private String message;
+    private LocalDateTime timestamp;
+    private T data;
+
+    public ApiResponse(int code, String message, LocalDateTime timestamp, T data) {
+        this.code = code;
+        this.message = message;
+        this.timestamp = timestamp;
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
