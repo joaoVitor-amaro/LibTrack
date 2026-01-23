8506b2c18d5ff77154722d51b74375a3b8fa0b17
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-01-22 23:24:25 -0300

    feat: adiciona entidade Aluno herdando de Usuario com o atributo turma

--- src/main/java/com/libTrack/LibTrack/model/Aluno.java
+++ src/main/java/com/libTrack/LibTrack/model/Aluno.java
@@ -1,5 +1,18 @@
 package com.libTrack.LibTrack.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
 public class Aluno extends Usuario{
+    @Column(name = "turma")
     private String turma;
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
 }
