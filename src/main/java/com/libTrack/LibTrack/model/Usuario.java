994d952b7b0dd53619a67e9f52761185ef497e3e
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-01-22 23:19:50 -0300

    feat: Adicionado as constraints nas colunas, os getters e setters e a nova coluna telefone

--- src/main/java/com/libTrack/LibTrack/model/Usuario.java
+++ src/main/java/com/libTrack/LibTrack/model/Usuario.java
@@ -1,12 +1,92 @@
 package com.libTrack.LibTrack.model;
 
 import com.libTrack.LibTrack.model.enums.TyperUser;
+import jakarta.persistence.*;
 
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "matricula", nullable = false, unique = true)
     private long matricula;
+    @Column(name = "nome", nullable = false)
     private String nome;
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+    @Column(name = "telefone")
+    private String telefone;
+    @Column(name = "login", unique = true)
     private String login;
+    @Column(name = "senha")
     private String senha;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "typeuser", nullable = false)
     private TyperUser typerUser;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(long matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public TyperUser getTyperUser() {
+        return typerUser;
+    }
+
+    public void setTyperUser(TyperUser typerUser) {
+        this.typerUser = typerUser;
+    }
 }
