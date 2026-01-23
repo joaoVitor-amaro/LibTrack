7d7da2e90e0b8bebb86da0d603c10410fee0c180
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-01-22 23:29:22 -0300

    feat: adiciona exceção personalizada para regras de negócio

---
+++ src/main/java/com/libTrack/LibTrack/exception/RegraNegocioException.java
@@ -0,0 +1,7 @@
+package com.libTrack.LibTrack.exception;
+
+public class RegraNegocioException extends RuntimeException{
+    public RegraNegocioException(String message) {
+        super(message);
+    }
+}
