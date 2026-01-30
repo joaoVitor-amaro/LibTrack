8706dceb423cbb2303ae25ade4999bc56bbfbd1e
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-01-30 11:02:25 -0300

    feat: Endpoint para cadastrar o aluno

---
+++ src/main/java/com/libTrack/LibTrack/controller/BibliotecarioController.java
@@ -0,0 +1,36 @@
+package com.libTrack.LibTrack.controller;
+
+import com.libTrack.LibTrack.dto.request.AlunoRequest;
+import com.libTrack.LibTrack.dto.response.AlunoResponse;
+import com.libTrack.LibTrack.dto.response.ApiResponse;
+import com.libTrack.LibTrack.service.BibliotecarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/bibliotecario")
+@CrossOrigin("http://localhost:4200")
+public class BibliotecarioController {
+    private final BibliotecarioService bibliotecarioService;
+
+    @Autowired
+    public BibliotecarioController(BibliotecarioService bibliotecarioService) {
+        this.bibliotecarioService = bibliotecarioService;
+    }
+
+    @PostMapping("/alunos")
+    public ResponseEntity<ApiResponse<AlunoResponse>> cadastroAluno(@RequestBody @Valid AlunoRequest alunoRequest) {
+        AlunoResponse aluno = this.bibliotecarioService.cadastrarBibliotecario(alunoRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(
+                HttpStatus.CREATED.value(),
+                "Aluno cadastrado",
+                LocalDateTime.now(),
+                aluno
+        ));
+    }
+}
