a37aa3fd26d627f7ea092a2f2d9deac8acec0faa
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-02-06 12:59:15 -0300

    feat: Adicionar Service de cadastrar o aluno que envia um email com token pra ativar a conta

--- src/main/java/com/libTrack/LibTrack/service/BibliotecarioService.java
+++ src/main/java/com/libTrack/LibTrack/service/BibliotecarioService.java
@@ -4,10 +4,13 @@ import com.libTrack.LibTrack.dto.request.AlunoRequest;
 import com.libTrack.LibTrack.dto.response.AlunoResponse;
 import com.libTrack.LibTrack.exception.RegraNegocioException;
 import com.libTrack.LibTrack.model.Aluno;
+import com.libTrack.LibTrack.model.TokenAtivacao;
 import com.libTrack.LibTrack.model.enums.StatusUser;
 import com.libTrack.LibTrack.model.enums.TyperUser;
 import com.libTrack.LibTrack.repository.AlunoRepository;
+import com.libTrack.LibTrack.repository.TokenAtivacaoRepository;
 import com.libTrack.LibTrack.repository.UsuarioRepository;
+import org.antlr.v4.runtime.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,14 +18,20 @@ import org.springframework.stereotype.Service;
 public class BibliotecarioService {
     private final UsuarioRepository usuarioRepository;
     private final AlunoRepository alunoRepository;
+    private final EmailService emailService;
+    private final TokenAtivacaoService tokenAtivacaoService;
+    private final TokenAtivacaoRepository tokenAtivacaoRepository;
 
     @Autowired
-    public BibliotecarioService(UsuarioRepository usuarioRepository, AlunoRepository alunoRepository) {
+    public BibliotecarioService(UsuarioRepository usuarioRepository, AlunoRepository alunoRepository, EmailService emailService, TokenAtivacaoService tokenAtivacaoService, TokenAtivacaoRepository tokenAtivacaoRepository) {
         this.usuarioRepository = usuarioRepository;
         this.alunoRepository = alunoRepository;
+        this.emailService = emailService;
+        this.tokenAtivacaoService = tokenAtivacaoService;
+        this.tokenAtivacaoRepository = tokenAtivacaoRepository;
     }
 
-    public AlunoResponse cadastrarBibliotecario(AlunoRequest alunoRequest) {
+    public AlunoResponse cadastrarAluno(AlunoRequest alunoRequest) {
         if(this.usuarioRepository.existsByMatricula(alunoRequest.getMatricula())) {
             throw new RegraNegocioException("Matricula já cadastrada");
         }
@@ -38,7 +47,15 @@ public class BibliotecarioService {
         aluno.setTurma(alunoRequest.getTurma());
         aluno.setTyperUser(TyperUser.ALUNO);
         aluno.setStatusUser(StatusUser.PENDENTE);
+        String token = this.tokenAtivacaoService.gerarToken();
+        this.emailService.sendEmail(
+                aluno.getEmail(),
+                "Ativação de conta",
+                "Clique no link: http://localhost:4200/ativar?token=" + token
+        );
+        TokenAtivacao tokenAtivacao = new TokenAtivacao(token, aluno);
         this.alunoRepository.save(aluno);
+        this.tokenAtivacaoRepository.save(tokenAtivacao);
         return new AlunoResponse(aluno);
     }
 }
