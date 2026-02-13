427dde24133fa85fad9660690d1569d7efe27787
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-02-13 20:27:38 -0300

    refactor: adiciona construtor em Aluno para receber AlunoRequest

--- src/main/java/com/libTrack/LibTrack/model/Aluno.java
+++ src/main/java/com/libTrack/LibTrack/model/Aluno.java
@@ -1,5 +1,8 @@
 package com.libTrack.LibTrack.model;
 
+import com.libTrack.LibTrack.dto.request.AlunoRequest;
+import com.libTrack.LibTrack.model.enums.StatusUser;
+import com.libTrack.LibTrack.model.enums.TyperUser;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
@@ -7,6 +10,13 @@ import jakarta.persistence.Entity;
 public class Aluno extends Usuario{
     @Column(name = "turma")
     private String turma;
+
+    public Aluno() {}
+
+    public Aluno(AlunoRequest aluno) {
+        super(aluno.getMatricula(), aluno.getNome(), aluno.getEmail(), aluno.getTelefone(), TyperUser.ALUNO, StatusUser.PENDENTE);
+        this.turma = aluno.getTurma();
+    }
 
     public String getTurma() {
         return turma;
